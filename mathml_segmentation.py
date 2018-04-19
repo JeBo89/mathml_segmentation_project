de93e52b56003de5c60f2a81799e58ca89822e5d
arash parnia <arashparnia@gmail.com>
2018-04-19 13:52:44 +0200

    generate trig file

--- mathml_segmentation.py
+++ mathml_segmentation.py
@@ -5,7 +5,9 @@ import regex as re
 import json
 from pprint import pprint
 import uuid
+from iribaker import to_iri
 
+from rdflib import Dataset, URIRef, Literal, Namespace, RDF, RDFS, OWL, XSD
 
 class mathml_segmentation:
     NS = ["http: // www.w3.org / 1998 / Math / MathML"]
@@ -142,7 +144,7 @@ def main():
     content = []
     # print ((data['results']['bindings'][0:2]))
     # exit(0)
-    for data_d in data['results']['bindings']:
+    for data_d in data['results']['bindings'][0:10]:
         mathml_xml_string = (data_d['Formula']["value"])
         # print (mathml_xml_string)
         mathml_xml = etree.fromstring(mathml_xml_string)
@@ -155,7 +157,6 @@ def main():
 
 def make_RDF(contents):
 
-    from rdflib import Dataset, URIRef, Literal, Namespace, RDF, RDFS, OWL, XSD
 
     host  = "http://localhost:5820/MATH"
     # A namespace for our resources
@@ -178,84 +179,40 @@ def make_RDF(contents):
 
     dataset.default_context.parse("vocab.ttl",format="turtle")
     # IRI baker is a library that reliably creates valid (parts of) IRIs from strings (spaces are turned into underscores, etc.).
-    from iribaker import to_iri
 
-    # Let's iterate over the dictionary, and create some triples
-    # Let's pretend we know exactly what the 'schema' of our CSV file is
+
     for row in contents:
-        # `id` is the primary key and we use it as our primary resource, but we'd also like to use it as a label
-        # print (row)
         id = URIRef((data + str(row['id']))) # primary key for the object
+        Mathematical_entity = Literal('Mathematical_entity', datatype=XSD['string'])
+        graph.add((id, RDFS.label,Mathematical_entity))
 
         mathematical_context = Literal(row['Mathematical_Context'], datatype=XSD['string'])
         graph.add((id, VOCAB['mathematical_context'], mathematical_context))
 
         id_ = URIRef((data + str(row['id_'])))
-        graph.add((id, VOCAB['previous_id'] ,id_))
+        graph.add((id, DATA['previous_id'] ,id_))
 
         if ('Formula' in row):
             formula = Literal(row['Formula'], datatype=XSD['string'])
-            dataset.add((id, VOCAB['formula'], formula))
+            graph.add((id, VOCAB['formula'], formula))
 
         if ('Symbol' in row):
             symbol = Literal(row['Symbol'], datatype=XSD['string'])
-            dataset.add((id, VOCAB['symbol'], symbol))
+            graph.add((id, VOCAB['symbol'], symbol))
             parent_id = URIRef((data + str(row['parent_id'])))
-            dataset.add((id, VOCAB['part_of'], parent_id))
+            graph.add((id, VOCAB['part_of'], parent_id))
 
 
         if ('Operator' in row):
             operator = Literal(row['Operator'], datatype=XSD['string'])
-            dataset.add((id, VOCAB['operator'], operator))
+            graph.add((id, VOCAB['operator'], operator))
             parent_id = URIRef((data + str(row['parent_id'])))
-            dataset.add((id, VOCAB['part_of'], parent_id))
+            graph.add((id, VOCAB['part_of'], parent_id))
 
-
-        # print
-        # person = URIRef(to_iri(data + row['Name']))
-        # name = Literal(row['Name'], datatype=XSD['string'])
-        # # `Country` is a resource
-        # country = URIRef(to_iri(data + row['Country']))
-        # # But we'd also like to use the name as a label (with a language tag!)
-        # country_name = Literal(row['Country'], lang='en')
-        # # `Age` is a literal (an integer)
-        # age = Literal(int(row['Age']), datatype=XSD['int'])
-        # # `Favourite Colour` is a resource
-        # colour = URIRef(to_iri(data + row['Favourite Colour']))
-        # colour_name = Literal(row['Favourite Colour'], lang='en')
-        # # `Place` is a resource
-        # place = URIRef(to_iri(data + row['Place']))
-        # place_name = Literal(row['Place'], lang='en')
-        # # `Address` is a literal (a string)
-        # address = Literal(row['Address'], datatype=XSD['string'])
-        # # `Hobby` is a resource
-        # hobby = URIRef(to_iri(data + row['Hobby']))
-        # hobby_name = Literal(row['Hobby'], lang='en')
-        #
-        # # All set... we are now going to add the triples to our graph
-        # graph.add((person, VOCAB['name'], name))
-        # graph.add((person, VOCAB['age'], age))
-        # graph.add((person, VOCAB['address'], address))
-        #
-        # # Add the place and its label
-        # graph.add((person, VOCAB['place'], place))
-        # graph.add((place, VOCAB['name'], place_name))
-        #
-        # # Add the country and its label
-        # graph.add((person, VOCAB['country'], country))
-        # graph.add((country, VOCAB['name'], country_name))
-        #
-        # # Add the favourite colour and its label
-        # graph.add((person, VOCAB['favourite_colour'], colour))
-        # graph.add((colour, VOCAB['name'], colour_name))
-        #
-        # # Add the hobby and its label
-        # graph.add((person, VOCAB['hobby'], hobby))
-        # graph.add((hobby, VOCAB['name'], hobby_name))
 
     with open('math_db.trig','w') as f:
         graph.serialize(f, format='trig')
-    # print (dataset.serialize(format='trig'))
+    print (dataset.serialize(format='trig'))
 
 if __name__ == '__main__':
     main()
