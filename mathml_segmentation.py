5c18d835bb8d0d01dfa7433b892a2d86f0a0770d
arash parnia <arashparnia@gmail.com>
2018-04-19 11:25:00 +0200

    making graph

--- mathml_segmentation.py
+++ mathml_segmentation.py
@@ -4,43 +4,55 @@ from lxml import etree
 import regex as re
 import json
 from pprint import pprint
+import uuid
 
 
 class mathml_segmentation:
     NS = ["http: // www.w3.org / 1998 / Math / MathML"]
-    counter = 0
+    current_id = 0
 
     def __init__(self):
-        self.counter = 0
+        self.new_id()
         return None
-
-    def parse_mathml(self,mathml_xml):
+    def new_id(self):
+        self.current_id = uuid.uuid1()
+        return self.current_id
+    def get_id(self):
+        return self.current_id
+    def parse_mathml(self,mathml_xml,math_context):
         l =[]
         if (mathml_xml.getchildren() != []):
             d = {}
             s = etree.tostring(mathml_xml, encoding="unicode", method="xml",xml_declaration=False).replace(" ", "").replace("\n", "")
             # s = re.sub(r'([\\n\s]*?)','', s, flags=re.IGNORECASE)
-            d["Formulas"] = s
-            d["id"] = mathml_segmentation.counter
-            mathml_segmentation.counter += 1
+            d["Formula"] = s
+            d["Mathematical_Context"] = math_context
+            d["id_"] = self.get_id()
+            d["id"] = self.new_id()
+            parent_id = d["id"]
+
             l.append(d)
         for children in mathml_xml.getchildren():
             if children.getchildren() != []:
                 # HAS CHILDREN
-                l = l + self.parse_mathml(children)
+                l = l + self.parse_mathml(children,math_context)
             else:
                 # NO CHILDREN
                 if children.tag == '{http://www.w3.org/1998/Math/MathML}mi':
                     d = {}
                     d["Symbol"] =  children.text
-                    d["id"] = mathml_segmentation.counter
-                    mathml_segmentation.counter += 1
+                    d["Mathematical_Context"] = math_context
+                    d["id_"] = self.get_id()
+                    d["id"] = self.new_id()
+                    d["parent_id"] = parent_id
                     l.append(d)
-                if children.tag == '{http://www.w3.org/1998/Math/MathML}mo':
+                elif children.tag == '{http://www.w3.org/1998/Math/MathML}mo':
                     d = {}
                     d['Operator'] =   children.text
-                    d["id"] = mathml_segmentation.counter
-                    mathml_segmentation.counter += 1
+                    d["Mathematical_Context"] = math_context
+                    d["id_"] = self.get_id()
+                    d["id"] = self.new_id()
+                    d["parent_id"] = parent_id
                     l.append(d)
 
         return l
@@ -128,21 +140,24 @@ def main():
 
     # pprint(data['results']['bindings'][0]['Formula'])
     content = []
-    for data_d in data['results']['bindings']:
+    # print ((data['results']['bindings'][0:2]))
+    # exit(0)
+    for data_d in data['results']['bindings'][0:1]:
         mathml_xml_string = (data_d['Formula']["value"])
         # print (mathml_xml_string)
         mathml_xml = etree.fromstring(mathml_xml_string)
-        content.append (ms.parse_mathml(mathml_xml))
+        math_context = data_d['Label']['value']
+        content = content +  (ms.parse_mathml(mathml_xml,math_context))
 
     pprint(content)
-    # make_RDF(contents)
+    make_RDF(content)
 
 
 def make_RDF(contents):
 
     from rdflib import Dataset, URIRef, Literal, Namespace, RDF, RDFS, OWL, XSD
 
-    host  = ""
+    host  = "http://localhost:5820/MATH"
     # A namespace for our resources
     data = host + '/resource/'
     DATA = Namespace(data)
@@ -151,12 +166,12 @@ def make_RDF(contents):
     VOCAB = Namespace(host + '/vocab/')
 
     # The URI for our graph
-    graph_uri = URIRef(host+ '/resource/examplegraph')
+    graph_uri = URIRef(host+ '/resource/mathentitygraph')
 
     # We initialize a dataset, and bind our namespaces
     dataset = Dataset()
-    dataset.bind('g20data', DATA)
-    dataset.bind('g20vocab', VOCAB)
+    dataset.bind('data', DATA)
+    dataset.bind('vocab', VOCAB)
 
     # We then get a new graph object with our URI from the dataset.
     graph = dataset.graph(graph_uri)
@@ -167,50 +182,79 @@ def make_RDF(contents):
     # Let's iterate over the dictionary, and create some triples
     # Let's pretend we know exactly what the 'schema' of our CSV file is
     for row in contents:
-        # `Name` is the primary key and we use it as our primary resource, but we'd also like to use it as a label
-        person = URIRef(to_iri(data + row['Name']))
-        name = Literal(row['Name'], datatype=XSD['string'])
-        # `Country` is a resource
-        country = URIRef(to_iri(data + row['Country']))
-        # But we'd also like to use the name as a label (with a language tag!)
-        country_name = Literal(row['Country'], lang='en')
-        # `Age` is a literal (an integer)
-        age = Literal(int(row['Age']), datatype=XSD['int'])
-        # `Favourite Colour` is a resource
-        colour = URIRef(to_iri(data + row['Favourite Colour']))
-        colour_name = Literal(row['Favourite Colour'], lang='en')
-        # `Place` is a resource
-        place = URIRef(to_iri(data + row['Place']))
-        place_name = Literal(row['Place'], lang='en')
-        # `Address` is a literal (a string)
-        address = Literal(row['Address'], datatype=XSD['string'])
-        # `Hobby` is a resource
-        hobby = URIRef(to_iri(data + row['Hobby']))
-        hobby_name = Literal(row['Hobby'], lang='en')
+        # `id` is the primary key and we use it as our primary resource, but we'd also like to use it as a label
+        # print (row)
+        id = URIRef((data + str(row['id']))) # primary key for the object
 
-        # All set... we are now going to add the triples to our graph
-        graph.add((person, VOCAB['name'], name))
-        graph.add((person, VOCAB['age'], age))
-        graph.add((person, VOCAB['address'], address))
+        mathematical_context = Literal(row['Mathematical_Context'], datatype=XSD['string'])
+        graph.add((id, VOCAB['mathematical_context'], mathematical_context))
 
-        # Add the place and its label
-        graph.add((person, VOCAB['place'], place))
-        graph.add((place, VOCAB['name'], place_name))
+        id_ = URIRef((data + str(row['id_'])))
+        graph.add((id, VOCAB['previous_id'] ,id_))
 
-        # Add the country and its label
-        graph.add((person, VOCAB['country'], country))
-        graph.add((country, VOCAB['name'], country_name))
+        if ('Formula' in row):
+            formula = Literal(row['Formula'], datatype=XSD['string'])
+            graph.add((id, VOCAB['formula'], formula))
 
-        # Add the favourite colour and its label
-        graph.add((person, VOCAB['favourite_colour'], colour))
-        graph.add((colour, VOCAB['name'], colour_name))
+        if ('Symbol' in row):
+            symbol = Literal(row['Symbol'], datatype=XSD['string'])
+            graph.add((id, VOCAB['symbol'], symbol))
+            parent_id = URIRef((data + str(row['parent_id'])))
+            graph.add((id, VOCAB['part_of'], parent_id))
 
-        # Add the hobby and its label
-        graph.add((person, VOCAB['hobby'], hobby))
-        graph.add((hobby, VOCAB['name'], hobby_name))
+
+        if ('Operator' in row):
+            operator = Literal(row['Operator'], datatype=XSD['string'])
+            graph.add((id, VOCAB['operator'], operator))
+            parent_id = URIRef((data + str(row['parent_id'])))
+            graph.add((id, VOCAB['part_of'], parent_id))
+
+
+        # print
+        # person = URIRef(to_iri(data + row['Name']))
+        # name = Literal(row['Name'], datatype=XSD['string'])
+        # # `Country` is a resource
+        # country = URIRef(to_iri(data + row['Country']))
+        # # But we'd also like to use the name as a label (with a language tag!)
+        # country_name = Literal(row['Country'], lang='en')
+        # # `Age` is a literal (an integer)
+        # age = Literal(int(row['Age']), datatype=XSD['int'])
+        # # `Favourite Colour` is a resource
+        # colour = URIRef(to_iri(data + row['Favourite Colour']))
+        # colour_name = Literal(row['Favourite Colour'], lang='en')
+        # # `Place` is a resource
+        # place = URIRef(to_iri(data + row['Place']))
+        # place_name = Literal(row['Place'], lang='en')
+        # # `Address` is a literal (a string)
+        # address = Literal(row['Address'], datatype=XSD['string'])
+        # # `Hobby` is a resource
+        # hobby = URIRef(to_iri(data + row['Hobby']))
+        # hobby_name = Literal(row['Hobby'], lang='en')
+        #
+        # # All set... we are now going to add the triples to our graph
+        # graph.add((person, VOCAB['name'], name))
+        # graph.add((person, VOCAB['age'], age))
+        # graph.add((person, VOCAB['address'], address))
+        #
+        # # Add the place and its label
+        # graph.add((person, VOCAB['place'], place))
+        # graph.add((place, VOCAB['name'], place_name))
+        #
+        # # Add the country and its label
+        # graph.add((person, VOCAB['country'], country))
+        # graph.add((country, VOCAB['name'], country_name))
+        #
+        # # Add the favourite colour and its label
+        # graph.add((person, VOCAB['favourite_colour'], colour))
+        # graph.add((colour, VOCAB['name'], colour_name))
+        #
+        # # Add the hobby and its label
+        # graph.add((person, VOCAB['hobby'], hobby))
+        # graph.add((hobby, VOCAB['name'], hobby_name))
 
     # with open('example-simple.trig','w') as f:
     #     graph.serialize(f, format='trig')
+    print (dataset.serialize(format='trig'))
 
 if __name__ == '__main__':
     main()
