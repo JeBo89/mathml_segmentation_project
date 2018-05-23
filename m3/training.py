53446b2437902420bc0f4a8154b10ba802090f77
arash parnia <arashparnia@gmail.com>
2018-05-24 00:08:12 +0200

    m3

--- m3/training.py
+++ m3/training.py
@@ -54,38 +54,80 @@ def make_set(x):
     return x
 
 df = pd.DataFrame.from_dict(l)
-df['formula_set'] = df.groupby('formula_label').apply(make_set)
+# df  = df.groupby('formula_label').apply(make_set)
+df = pd.DataFrame({'symbol_set' : df.groupby('formula_label').apply(make_set)}).reset_index()
 
 # print (df.groupby('formula_label').agg('count').sort_values(by=['symbol_label'], ascending=False)) #shows how many symbols in each group
 # print(df)
 
+def get_broader(subject):
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery('''
+        prefix dbc: <http://dbpedia.org/resource/Category:>
+     prefix dct: <http://purl.org/dc/terms/>
+     prefix owl: <http://www.w3.org/2002/07/owl#>
+     prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+     prefix skos: <http://www.w3.org/2004/02/skos/core#>
+     prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+     prefix dbo: <http://dbpedia.org/ontology/>
+     select distinct ?broader_subject_label  WHERE
+     {
+       	?s rdfs:label "''' + subject + '''"@en;
+       	    skos:broader ?broader_subject.
+        
+        ?broader_subject rdfs:label ?broader_subject_label . filter(lang(?broader_subject_label) = "en")
+    }limit 10
+     ''')
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        the_result = result["broader_subject_label"]["value"]
+        print 'the_result  ', the_result
+        if (('sciences' in the_result.lower()) or
+                ('fields' in the_result.lower()) or
+                ('theories' in the_result.lower()) or
+                ('branches') in the_result.lower() or
+                ('studies' in the_result.lower())):
+            return the_result
+        elif (('main topic classifications' in the_result.lower()) or
+                  ('academic disciplines' in the_result.lower()) or
+                  ('wikipedia' in the_result.lower()) or
+                  ('Contents' in the_result.lower()) or
+                  ('Scientific disciplines') in the_result.lower()):
+            return subject
+        else:
+            get_broader(the_result)
+
 def get_subject(row):
-    print row
-    # sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    # sparql.setQuery("""
-    #    prefix dbc: <http://dbpedia.org/resource/Category:>
-    # prefix dct: <http://purl.org/dc/terms/>
-    # prefix owl: <http://www.w3.org/2002/07/owl#>
-    # prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    # prefix skos: <http://www.w3.org/2004/02/skos/core#>
-    # prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    # prefix dbo: <http://dbpedia.org/ontology/>
-    # select ?subject_Label WHERE
-    # {
-    #   	?s rdfs:label "Stokes' law"@en;
-    #   		dct:subject  ?subject .
-    #
-    #   ?subject rdfs:label ?subject_Label . filter(lang(?subject_Label) = "en")
-    # }limit 10
-    # """)
-    # sparql.setReturnFormat(JSON)
-    # results = sparql.query().convert()
-    #
-    # l = []
-    # for result in results["results"]["bindings"]:
-    #     subject = result["subject_Label"]["value"]
-    #     print subject
-    #     return  subject
+    # print row
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery('''
+       prefix dbc: <http://dbpedia.org/resource/Category:>
+    prefix dct: <http://purl.org/dc/terms/>
+    prefix owl: <http://www.w3.org/2002/07/owl#>
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    prefix skos: <http://www.w3.org/2004/02/skos/core#>
+    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    prefix dbo: <http://dbpedia.org/ontology/>
+    select Distinct ?subject_Label WHERE
+    {
+      	?s rdfs:label "''' + row +'''"@en;
+      		dct:subject  ?subject .
+
+      ?subject rdfs:label ?subject_Label . filter(lang(?subject_Label) = "en")
+    }limit 1
+    ''')
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        subject = result["subject_Label"]["value"]
+
+        broader = get_broader(subject=subject)
+        print '-------------------------------'
+        print 'broader = ' , broader
+        return  broader
 
     return None
 
@@ -96,5 +138,7 @@ def get_subject(row):
 
 
 
-print df
-# df['subject'] = df['formula_label'].apply(get_subject )
+# print df.head()
+df['subject'] = df['formula_label'].apply(get_subject )
+print df.head()
+df.to_csv('formula_with_subject.csv',encoding='utf-8')
